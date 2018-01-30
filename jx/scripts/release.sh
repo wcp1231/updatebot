bac5c28c0663ba466e76679899a58d2cf0601de1
James Strachan <james.strachan@gmail.com>
2018-01-30 16:27:03 +0000

    enable the release jx commands

--- jx/scripts/release.sh
+++ jx/scripts/release.sh
@@ -14,8 +14,7 @@ mvn versions:set -DnewVersion=${VERSION}
 mvn clean -B
 mvn -V -B -e -U install org.sonatype.plugins:nexus-staging-maven-plugin:1.6.7:deploy -P release -P openshift -DnexusUrl=https://oss.sonatype.org -DserverId=oss-sonatype-staging
 
-# now the sonatype repo ids will be on disk
-
-#jx step nexus release
-#jx step tag --version ${VERSION}
+# now release the sonatype staging repo
+jx step nexus release
+jx step tag --version ${VERSION}
 
